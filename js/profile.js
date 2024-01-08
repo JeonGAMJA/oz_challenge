393d6809e3f27cf506832aec83e5f46c67383632
gamjaJeon <indian_potato@naver.com>
2024-01-08 10:19:46 +0900

    feat: blog

---
+++ js/profile.js
@@ -0,0 +1,27 @@
+const profile = document.getElementById("profile");
+const profileCloseBtn = document.getElementById("profile-close-btn");
+const profileBackground = document.getElementById("profile-background");
+const profileBtn = document.getElementById("blog-navbar-profile");
+
+function closeProfile(event) {
+  profile.classList.replace("on-profile", "off-profile");
+  profileBackground.classList.replace("visible", "hidden");
+}
+
+function onProfile(event) {
+  if (profile.classList.contains("off-profile")) {
+    profile.classList.replace("off-profile", "on-profile");
+    profileBackground.classList.replace("hidden", "visible");
+  }
+}
+
+function checkProfileClass(event) {
+  if (profile.classList.contains("on-profile") == false) {
+    profile.classList.add("on-profile");
+    profileBackground.classList.add("visible");
+  }
+}
+
+profileBtn.addEventListener("click", onProfile);
+profileBtn.addEventListener("click", checkProfileClass);
+profileCloseBtn.addEventListener("click", closeProfile);
