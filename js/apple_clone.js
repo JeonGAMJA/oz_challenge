d56291f7e92f44eb11ad17cbd3b9bd99d0057f7a
soy <indian_potato@naver.com>
2024-01-16 14:12:07 +0900

    js:challenge1

--- js/apple_clone.js
+++ js/apple_clone.js
@@ -1,2 +1,10 @@
-let globalnavList = document.getElementsByClassName("globalnav-list");
-let globalnavMenu = document.getElementsByClassName("globalnav-menu");
+const globalNavMenu = document.getElementById("globalnav-menu");
+const globalNavMenuTrigger = document.getElementById(
+  "globalnav-menutrigger-button"
+);
+
+function globalNavWithFlyoutOpen(event) {
+  globalNavMenu.style.visibility = visible;
+}
+
+globalNavMenuTrigger.addEventListener("click", globalNavWithFlyoutOpen);
