735298b63cf81c2d5d47ccf1575d3a164887176e
soy <indian_potato@naver.com>
2024-01-18 20:44:38 +0900

    feat: js challenge signin-form2

---
+++ js/challenge/signin-form2/signin-form.js
@@ -0,0 +1,16 @@
+const userId = document.getElementById("user-id-input");
+const userName = document.getElementById("user-name-input");
+const userPhone = document.getElementById("user-phone-input");
+const userPosition = document.getElementById("user-position-input");
+
+function printUserInfo(event) {
+  event.preventDefault();
+  alert(`${userId}님 환영합니다.`);
+  alert(
+    `회원가입시 입력하신 내역은 다음과 같습니다.\n 아이디 : ${userId.value}\n 이름 : ${userName.value}\n 전화번호 : ${userPhone.value}\n 원하는 직무 : ${userPosition.value}`
+  );
+}
+
+function reset() {
+  location.reload(true);
+}
