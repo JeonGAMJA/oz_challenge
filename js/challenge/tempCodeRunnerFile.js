f4947ecfc52dfef10983646e48f9b1ad6c033b39
GamjaJeon <indian_potato@naver.com>
2024-01-25 17:17:50 +0900

    feat: js challenge lucky

--- js/challenge/tempCodeRunnerFile.js
+++ js/challenge/tempCodeRunnerFile.js
@@ -1,15 +1,2 @@
-
-// for (let i = 0; i < 5; i++) {
-//   let star = "";
-//   for (let j = 0; j < i; j++) {
-//     star += "*";
-//     console.log(star);
-//   }
-// }
-// for (let i = 4; i >= 0; i--) {
-//   let star = "";
-//   for (let j = 0; j < i; j++) {
-//     star += "*";
-//     console.log(star);
-//   }
-// }
+  let winningCount = 0;
+  const prize = winningCount * 10000;
